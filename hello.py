1160460ac2ad6146072fa15984687cb0121f8d44
yuuttana1223 <64090349+yuuttana1223@users.noreply.github.com>
2023-10-21 15:29:52 +0900

    ✨ HTMLのエスケープ処理

--- hello.py
+++ hello.py
@@ -3,6 +3,8 @@
 ユーザーが "Hello, World!" を表示するページにアクセスできるようになります。
 """
 
+from markupsafe import escape
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -13,3 +15,10 @@ def hello_world():
     この関数は単純なHTML文字列を戻します。
     """
     return "<p>Hello, World!</p>"
+
+@app.route("/<name>")
+def hello(name):
+    """
+    HTMLのエスケープ処理
+    """
+    return f"Hello, {escape(name)}!"
