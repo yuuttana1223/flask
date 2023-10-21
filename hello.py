90846133ea227110e7bb90f7c911ec816c5c77ec
yuuttana1223 <64090349+yuuttana1223@users.noreply.github.com>
2023-10-21 16:33:26 +0900

    ✨ スラッシュの振る舞いを検証

--- hello.py
+++ hello.py
@@ -1,6 +1,5 @@
 """
 このモジュールは、基本的な Flask アプリケーションを提供します。
-ユーザーが "Hello, World!" を表示するページにアクセスできるようになります。
 """
 
 from markupsafe import escape
@@ -26,13 +25,27 @@ def show_user_profile(username):
 @app.route('/post/<int:post_id>')
 def show_post(post_id):
     """
-    show the post with the given id, the id is an integer
+    int以外が来たら404
     """
     return f'Post {post_id}'
 
 @app.route('/path/<path:subpath>')
 def show_subpath(subpath):
     """
-    show the subpath after /path/
+    パスや文字列を受け取れる
     """
     return f'Subpath {escape(subpath)}'
+
+@app.route('/projects/')
+def projects():
+    """
+    リソースがコレクションまたはディレクトリの場合（例：ブログのカテゴリ、プロジェクトリストなど）は、URLの最後にスラッシュ(/)をつけると良いでしょう。これは、コレクションやディレクトリは通常、その下に他のリソースが存在し、その集合を示すものと見なされるためです。
+    """
+    return 'The project page'
+
+@app.route('/about')
+def about():
+    """
+    リソースが特定のページや項目（例：特定のブログ記事、特定のプロジェクトなど）を指す場合、URLの最後にスラッシュをつけないと良いでしょう。これは、そのURLが一つの特異なリソースを指すものと見なされ、その下に他のリソースが存在しないと想定されるためです。
+    """
+    return 'The about page'
