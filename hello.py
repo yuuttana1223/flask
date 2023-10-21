9422b899c8b91993d65103eac7c16ad28933f19a
yuuttana1223 <64090349+yuuttana1223@users.noreply.github.com>
2023-10-21 16:25:03 +0900

    ✨ URLに変数を設定し、コンバーターを使用

--- hello.py
+++ hello.py
@@ -16,9 +16,23 @@ def hello_world():
     """
     return "<p>Hello, World!</p>"
 
-@app.route("/<name>")
-def hello(name):
+@app.route('/user/<username>')
+def show_user_profile(username):
     """
-    HTMLのエスケープ処理
+    show the user profile for that user
     """
-    return f"Hello, {escape(name)}!"
+    return f'User {escape(username)}'
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    """
+    show the post with the given id, the id is an integer
+    """
+    return f'Post {post_id}'
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    """
+    show the subpath after /path/
+    """
+    return f'Subpath {escape(subpath)}'
